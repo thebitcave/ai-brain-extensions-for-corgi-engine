5dd9be827c1007f95bf35b554ab69a75c9d0b651
Marco Secchi <loziosecchi@gmail.com>
2019-10-29 12:12:08 +0100

    Update brain generator.

--- Assets/CorgiExtensions/AI/AIBrainGenerator.cs
+++ Assets/CorgiExtensions/AI/AIBrainGenerator.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.CorgiEngine;
 using UnityEngine;
 using MoreMountains.Tools;
 using UnityEngine.UIElements;
@@ -11,9 +13,60 @@ namespace TheBitCave.CorgiExensions.AI
 
         public AIBrainGraph aiBrainGraph;
 
+        public bool brainActive = true;
+
+        [Header("Frequencies")]
+        public float actionsFrequency = 0;
+        public float decisionFrequency = 0;
+
+        private void Start()
+        {
+            Cleanup();
+        }
+
         public void Generate()
         {
-    
+            Cleanup();
+
+            GenerateActions();
+            GenerateDecisions();
+            GenerateBrain();
+        }
+
+        private void GenerateDecisions()
+        {
+//            throw new NotImplementedException();
+        }
+
+        private void GenerateActions()
+        {
+//            throw new NotImplementedException();
+        }
+
+        private void GenerateBrain()
+        {
+            AIBrain brain = gameObject.AddComponent<AIBrain>();
+            brain.BrainActive = brainActive;
+            brain.ActionsFrequency = actionsFrequency;
+            brain.DecisionFrequency = decisionFrequency;
+        }
+
+        private void Cleanup()
+        {
+            AIBrain brain = GetComponent<AIBrain>();
+            Debug.Log(brain);
+            DestroyImmediate(brain);
+
+            foreach (var aiDecision in GetComponents<AIDecision>())
+            {
+                DestroyImmediate(aiDecision);
+            }
+            
+            foreach (var aiAction in GetComponents<AIAction>())
+            {
+                DestroyImmediate(aiAction);
+            }
+
         }
 
     }
---
+++ Assets/CorgiExtensions/AI/Editor/AIBrainGeneratorEditor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace TheBitCave.CorgiExensions.AI
+{
+    [CustomEditor (typeof(AIBrainGenerator))]
+    public class AIBrainGeneratorEditor : Editor
+    {
+        
+        protected SerializedProperty _brainActive;
+        protected SerializedProperty _actionsFrequency;
+        protected SerializedProperty _decisionFrequency;
+
+        private AIBrainGenerator _generator;
+        
+        private void OnEnable()
+        {
+            _generator = target as AIBrainGenerator;
+
+            _brainActive = serializedObject.FindProperty("brainActive");
+            _actionsFrequency = serializedObject.FindProperty("actionsFrequency");
+            _decisionFrequency = serializedObject.FindProperty("decisionFrequency");
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.HelpBox("Lorem Ipsum", MessageType.Info);
+            
+            EditorGUILayout.PropertyField(_brainActive);
+            EditorGUILayout.PropertyField(_actionsFrequency);
+            EditorGUILayout.PropertyField(_decisionFrequency);
+            serializedObject.ApplyModifiedProperties();
+
+            if(GUILayout.Button("Generate"))
+            {
+                _generator.Generate();
+            }
+        }
+    }
+}
