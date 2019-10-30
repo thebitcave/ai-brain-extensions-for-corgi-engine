fff51180a7ae3a12c79502f241b5370005379eac
Marco Secchi <loziosecchi@gmail.com>
2019-10-30 12:24:45 +0100

    Add new actions.

---
+++ Assets/CorgiExtensions/AI/Nodes/Actions/AIActionJumpNode.cs
@@ -0,0 +1,25 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace TheBitCave.CorgiExensions.AI
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="MoreMountains.CorgiEngine.AIActionJump"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Jump")]
+    public class AIActionJumpNode : AIActionNode
+    {
+        public int numberOfJumps = 1;
+
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionJump>();
+            action.Label = label;
+            action.NumberOfJumps = numberOfJumps;
+            return action;
+        }
+
+    }
+
+}
