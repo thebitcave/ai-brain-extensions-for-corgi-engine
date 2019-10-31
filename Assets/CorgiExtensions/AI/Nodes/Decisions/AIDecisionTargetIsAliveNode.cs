6d0f01913e2d920f9208a3a9f5f1f8139d99b90e
Marco Secchi <loziosecchi@gmail.com>
2019-10-31 21:25:02 +0100

    Add Decisions.

---
+++ Assets/CorgiExtensions/AI/Nodes/Decisions/AIDecisionTargetIsAliveNode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+namespace TheBitCave.CorgiExensions.AI
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="MoreMountains.CorgiEngine.AIDecisionTargetIsAlive"/> decision.
+    /// </summary>
+    [CreateNodeMenu("AI/Decision/Target Is Alive")]
+    public class AIDecisionTargetIsAliveNode : AIDecisionNode
+    {
+        public override AIDecision AddDecisionComponent(GameObject go)
+        {
+            var decision = go.AddComponent<AIDecisionTargetIsAlive>();
+            return decision;
+        }
+    }
+}
---
+++ Assets/CorgiExtensions/AI/Nodes/Decisions/AIDecisionTargetIsNullNode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+namespace TheBitCave.CorgiExensions.AI
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="MoreMountains.CorgiEngine.AIDecisionTargetIsNull"/> decision.
+    /// </summary>
+    [CreateNodeMenu("AI/Decision/Target Is Null")]
+    public class AIDecisionTargetIsNullNode : AIDecisionNode
+    {
+        public override AIDecision AddDecisionComponent(GameObject go)
+        {
+            var decision = go.AddComponent<AIDecisionTargetIsNull>();
+            decision.Label = label;
+            return decision;
+        }
+    }
+}
