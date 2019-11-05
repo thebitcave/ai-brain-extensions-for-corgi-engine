205c95f39c556a8cd2db1a114bebf26757728fc2
Marco Secchi <loziosecchi@gmail.com>
2019-11-05 11:33:57 +0100

    Add AIDecision node editors.

---
+++ Assets/CorgiExtensions/AI/Nodes/Decisions/Editor/AIDecisionDetectTargetRadiusNodeEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using XNodeEditor;
+
+namespace TheBitCave.CorgiExensions.AI
+{
+    [CustomNodeEditorAttribute(typeof(AIDecisionDetectTargetRadiusNode))]
+    public class AIDecisionDetectTargetRadiusNodeEditor : AIDecisionNodeEditor
+    {
+        private SerializedProperty _radius;
+        private SerializedProperty _detectionOriginOffset;
+        private SerializedProperty _targetLayer;
+        
+        public override void OnBodyGUI()
+        {
+            base.OnBodyGUI();
+            
+            _radius = serializedObject.FindProperty("radius");
+            _detectionOriginOffset = serializedObject.FindProperty("detectionOriginOffset");
+            _targetLayer = serializedObject.FindProperty("targetLayer");
+
+            serializedObject.Update();
+            EditorGUIUtility.labelWidth = 120;
+            NodeEditorGUILayout.PropertyField(_radius);
+            NodeEditorGUILayout.PropertyField(_detectionOriginOffset);
+            NodeEditorGUILayout.PropertyField(_targetLayer);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
