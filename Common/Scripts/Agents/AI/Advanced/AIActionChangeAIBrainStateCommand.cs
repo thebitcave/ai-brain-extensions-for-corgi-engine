f42b9f59ed932e62e2b4d55c778a3e3c4d884850
Marco Secchi <loziosecchi@gmail.com>
2019-12-21 18:36:25 +0100

    Refactor folder structure.

---
+++ Common/Scripts/Agents/AI/Advanced/AIActionChangeAIBrainStateCommand.cs
@@ -0,0 +1,28 @@
+﻿using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.Tools;
+using UnityEngine;
+
+namespace TheBitCave.CorgiExensions.AI
+{
+    /// <summary>
+    /// This action sends a change state command to all registered slave brains
+    /// </summary>
+    public class AIActionChangeAIBrainStateCommand : AIAction
+    {
+        [Header("Master Brain Settings")]
+        // The channel used to filter which slave should perform the action.
+        public string ChannelName;
+
+        // The state the slave should enter in.
+        public string StateName;
+
+        /// <summary>
+        /// On PerformAction we send a command to all brain slaves
+        /// </summary>
+        public override void PerformAction()
+        {
+            var evt = new ChangeAIBrainStateCommandEvent(ChannelName, StateName, _brain.Target);
+            MMEventManager.TriggerEvent(evt);
+        }
+    }
+}
---
+++ Common/Scripts/Agents/AI/Graph/Actions/AIActionChangeAIBrainStateCommandNode.cs
@@ -0,0 +1,26 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.CorgiExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="TheBitCave.MMToolsExtensions.AI.AIActionChangeAIBrainStateCommand"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Extensions/Change AIBrain State Command")]
+    public class AIActionChangeAIBrainStateCommandNode : AIActionNode
+    {
+        public string channelName;
+        public string stateName;
+
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionChangeAIBrainStateCommand>();
+            action.Label = label;
+            action.ChannelName = channelName;
+            action.StateName = stateName;
+            return action;
+        }
+    }
+}
