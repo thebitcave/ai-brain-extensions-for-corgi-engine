2cad750ceeed3241895df5bd902e4d7eee8d3048
marcosecchi <loziosecchi@gmail.com>
2020-06-09 15:21:49 +0200

    Add AIActionDashNode. Add AIActionMoveAwayFromTargetNode.

---
+++ Common/Scripts/Agents/AI/Graph/Actions/AIActionDashNode.cs
@@ -0,0 +1,21 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.CorgiExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="MoreMountains.CorgiEngine.AIActionDash"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Dash")]
+    public class AIActionDashNode : AIActionNode
+    {
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionDash>();
+            action.Label = label;
+            return action;
+        }
+    }
+}
---
+++ Common/Scripts/Agents/AI/Graph/Actions/AIActionMoveAwayFromTargetNode.cs
@@ -0,0 +1,21 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.CorgiExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="MoreMountains.CorgiEngine.AIActionMoveAwayFromTarget"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Move Away From Target")]
+    public class AIActionMoveAwayFromTargetNode : AIActionNode
+    {
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionMoveAwayFromTarget>();
+            action.Label = label;
+            return action;
+        }
+    }
+}
