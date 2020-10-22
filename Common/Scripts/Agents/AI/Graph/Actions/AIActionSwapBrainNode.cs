5b8a95b3a87eb8d621937badc41d2814a5958f7c
marcosecchi <loziosecchi@gmail.com>
2020-10-22 16:06:28 +0200

    Add AIActionSwapBrainNode.

---
+++ Common/Scripts/Agents/AI/Graph/Actions/AIActionSwapBrainNode.cs
@@ -0,0 +1,25 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEngine;
+
+namespace TheBitCave.CorgiExensions.AI.Graph
+{
+    /// <summary>
+    /// A node representing a Corgi <see cref="MoreMountains.CorgiEngine.AIActionSwapBrain"/> action.
+    /// </summary>
+    [CreateNodeMenu("AI/Action/Swap Brain")]
+    public class AIActionSwapBrainNode : AIActionNode
+    {
+        public AIBrain newAIBrain;
+        
+        public override AIAction AddActionComponent(GameObject go)
+        {
+            var action = go.AddComponent<AIActionSwapBrain>();
+            action.Label = label;
+            action.NewAIBrain = newAIBrain;
+            return action;
+        }
+
+    }
+}
---
+++ Common/Scripts/Agents/AI/Graph/Actions/Editor/AIActionSwapBrainNodeEditor.cs
@@ -0,0 +1,21 @@
+﻿using TheBitCave.MMToolsExtensions.AI.Graph;
+using UnityEditor;
+using XNodeEditor;
+
+namespace TheBitCave.CorgiExensions.AI.Graph
+{
+    [CustomNodeEditor(typeof(AIActionSwapBrainNode))]
+    public class AIActionSwapBrainNodeEditor : AIActionNodeEditor
+    {
+        private SerializedProperty _newAIBrain;
+        
+        protected override void SerializeAdditionalProperties()
+        {
+            _newAIBrain = serializedObject.FindProperty("newAIBrain");
+
+            serializedObject.Update();
+            NodeEditorGUILayout.PropertyField(_newAIBrain);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
